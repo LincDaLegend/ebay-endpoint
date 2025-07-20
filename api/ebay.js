44726a129cef64105aaccb2e15d4e53ad4ab3f42
LincDaLegend <lincdalegend@gmail.com>
2025-07-20 16:51:04 +0800

    Update ebay.js

--- api/ebay.js
+++ api/ebay.js
@@ -1,22 +1,22 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    let body = {};
+    try {
+      const body = typeof req.body === 'string'
+        ? JSON.parse(req.body)
+        : req.body;
 
-    // Try to parse JSON manually
-    if (req.headers['content-type'] === 'application/json') {
-      try {
-        body = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
-      } catch (e) {
-        return res.status(400).send('Invalid JSON');
+      if (body && body.challengeCode) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(200).json({
+          challengeResponse: body.challengeCode
+        });
       }
-    }
 
-    if (body.challengeCode) {
-      return res.status(200).json({ challengeResponse: body.challengeCode });
+      return res.status(200).send('OK');
+    } catch (error) {
+      return res.status(400).json({ error: 'Invalid request body' });
     }
-
-    return res.status(200).send('OK');
   } else {
-    res.status(405).send('Method Not Allowed');
+    return res.status(405).send('Method Not Allowed');
   }
 }
